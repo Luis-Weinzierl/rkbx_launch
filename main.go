4380c2fe6aff6de05ba5577b45556136e6345a8d
Luis Weinzierl <chronicalcoding@gmail.com>
2026-03-26 21:24:30 +0100

    Added Version selector

--- main.go
+++ main.go
@@ -79,6 +79,7 @@ func main() {
 		container.NewVBox(
 			widgets.NewHeader("Configuration"),
 			widgets.NewSubheader("General"),
+			widgets.NewSelectEntry("Rekordbox Version", &config.Keeper_rekordboxVersion, []string{"7.2.10", "7.2.8", "7.2.6", "7.2.4", "7.2.3", "7.2.2", "7.1.4"}),
 			widgets.NewBoolConfig("Auto-Update", &config.App_autoUpdate),
 			widgets.NewBoolConfig("Debug Mode", &config.App_debug),
 			widgets.NewBoolConfig("Keep non-master decks warm", &config.Keeper_keepWarm),
